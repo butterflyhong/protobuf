8fa2577b2f2458e262c4e1a53381a61411ad231d
Zhang Hong <zhanghong@kuaishou.com>
2017-08-25 23:14:11 +0800

    fix package import error

--- protoc-gen-go/main.go
+++ protoc-gen-go/main.go
@@ -52,8 +52,8 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/protoc-gen-go/generator"
+	"github.com/butterflyhong/protobuf/proto"
+	"github.com/butterflyhong/protobuf/protoc-gen-go/generator"
 )
 
 func main() {
